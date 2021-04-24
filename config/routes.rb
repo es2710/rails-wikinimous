9186dae0d3e48f936fe0c30864fdf6a1bbd95c54
Ed Sants <edward.sants@googlemail.com>
2021-04-24 08:25:06 +0100

    wiki part complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Create
+  get '/articles/new', to: 'articles#new'
+  post 'articles', to: 'articles#create'
+
+  # Read
+  get '/articles', to: 'articles#index'
+
+  get 'articles/:id', to: 'articles#show', as: :article
+
+  # Update
+  get '/articles/:id/edit', to: 'articles#edit'
+  patch '/articles/:id', to: 'articles#update'
+
+  # Delete
+
+  delete 'article/:id', to 'articles#destroy'
 end
